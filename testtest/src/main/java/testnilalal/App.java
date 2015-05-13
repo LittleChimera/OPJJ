b7f0c72458333e325c8307d0b08616a22537a3f7
lukadante <luka.dante3@gmail.com>
2015-05-13 17:58:19 +0200

    Addded testtest

---
+++ testtest/src/main/java/testnilalal/App.java
@@ -0,0 +1,13 @@
+package testnilalal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
