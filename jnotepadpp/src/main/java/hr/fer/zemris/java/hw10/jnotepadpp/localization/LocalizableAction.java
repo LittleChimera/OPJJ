7652058d7396742bc924c4e05b273a95cdc6e6bd
Luka Skugor <luka.dante3@gmail.com>
2015-05-22 18:16:07 +0200

    improved localization

--- jnotepadpp/src/main/java/hr/fer/zemris/java/hw10/jnotepadpp/localization/LocalizableAction.java
+++ jnotepadpp/src/main/java/hr/fer/zemris/java/hw10/jnotepadpp/localization/LocalizableAction.java
@@ -17,7 +17,7 @@ public abstract class LocalizableAction extends AbstractAction {
 	/**
 	 * Creates a new LocalizableAction which determines name and description of
 	 * the action by the provided key and localization provider. Keys are
-	 * defined as key_Action.{PROPERTY}.
+	 * defined as key_[Action.{PROPERTY}] (all lower case).
 	 * 
 	 * @param key
 	 *            key of the action
@@ -29,7 +29,8 @@ public abstract class LocalizableAction extends AbstractAction {
 		super();
 		
 		localizationProvider.addLocalizationListener(() -> {
-			putValue(Action.NAME, localizationProvider.getString(key));
+			putValue(Action.NAME, localizationProvider.getString(key + "_name"));
+			putValue(Action.SHORT_DESCRIPTION, localizationProvider.getString(key + "_short_description"));
 		});
 	}
 
